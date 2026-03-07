c9957666853e4a645f372c435f07dc3e7e4f6cf5
melih <meljhdurmus@gmail.com>
2026-03-08 00:10:22 +0300

    228-ultra fast mathmetician

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string a, b;
+  cin >> a >> b;
+
+  for (int i = 0; i<a.size();  i++) {
+    if(a[i] != b[i]) {
+      cout <<1;
+    } else {
+      cout <<0;
+    }
+  }
+
+  return 0;
+
+}
