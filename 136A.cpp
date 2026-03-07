08591090837e968a436ca898e14ca85401ef03f0
melih <meljhdurmus@gmail.com>
2026-03-07 22:49:42 +0300

    136a-presents

---
+++ 136A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int p[101], ans[101];
+
+    for (int i = 1; i <= n; i++)
+        cin >> p[i];
+
+    // p[i] = i, j'ye verdi → ans[j] = i
+    for (int i = 1; i <= n; i++)
+        ans[p[i]] = i;
+
+    for (int i = 1; i <= n; i++)
+        cout << ans[i] << " ";
+
+    return 0;
+}
